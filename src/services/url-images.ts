6f9b8df0a397139ab5b53f5cb5d7ed34a772a1aa
yhKim99 <yonghoon.kim99@gmail.com>
2023-10-18 14:05:43 -0400

    get the cropped image

--- src/services/url-images.ts
+++ src/services/url-images.ts
@@ -2,7 +2,6 @@
 const getCroppedImage = (url: string) => {
     const index = url.indexOf('media/');
     const newUrl = url.slice(0, index + 6) + "crop/600/400/" + url.slice(index + 6, url.length);
-    console.log(newUrl);
     return newUrl;
 }
 export default getCroppedImage;
