2b1ebf3c0227747f5e57c8be84fadbf7412607fd
yhKim99 <yonghoon.kim99@gmail.com>
2023-10-18 15:04:16 -0400

    fix duplicate style

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius="10" overflow="hidden">
+    <Card>
       <Img src={getCroppedImage(game.background_image)}></Img>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,6 +4,7 @@ import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGame from "../hooks/useGame";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, errors, loading } = useGame();
@@ -15,11 +16,15 @@ const GameGrid = () => {
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10}>
         {loading &&
           skeleton.map((skeleton) => (
-            <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+            </GameCardContainer>
           ))}
         {games.map((games) => (
           <>
-            <GameCard key={games.id} game={games}></GameCard>
+            <GameCardContainer>
+              <GameCard key={games.id} game={games}></GameCard>
+            </GameCardContainer>
           </>
         ))}
       </SimpleGrid>
