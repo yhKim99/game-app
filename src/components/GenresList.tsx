afef5ac75f815d5297097a6ad9bbcb97065921cd
yhKim99 <yonghoon.kim99@gmail.com>
2023-10-22 11:36:29 -0400

    filter the game

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,8 +1,20 @@
-import { List, ListItem, Image, HStack, Text, Spinner } from "@chakra-ui/react";
+import {
+  List,
+  ListItem,
+  Image,
+  HStack,
+  Text,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImage from "../services/url-images";
 
-const GenresList = () => {
+interface Prop {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenresList = ({ onSelectGenre }: Prop) => {
   const { data, loading, errors } = useGenres();
 
   return (
@@ -11,14 +23,20 @@ const GenresList = () => {
       {loading && <Spinner marginLeft="5"></Spinner>}
       <List>
         {data.map((genre) => (
-          <ListItem>
+          <ListItem marginBottom="3" key={genre.id}>
             <HStack>
               <Image
                 borderRadius="8"
                 boxSize="32px"
                 src={getCroppedImage(genre.image_background)}
               ></Image>
-              <Text fontSize="lg">{genre.name}</Text>
+              <Button
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant="link"
+              >
+                {genre.name}
+              </Button>
             </HStack>
           </ListItem>
         ))}
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,4 +1,4 @@
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import appClients from "../services/app-clients";
 
@@ -7,7 +7,7 @@ interface FetchResponse<T> {
     results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [errors, setErrors] = useState("");
     const [loading, setLoading] = useState(false);
@@ -18,7 +18,7 @@ const useData = <T>(endpoint: string) => {
 
         setLoading(true);
       appClients
-        .get<FetchResponse<T>>(endpoint, {signal: controller.signal})
+        .get<FetchResponse<T>>(endpoint, {signal: controller.signal, ...requestConfig})
         .then((res) => {
             setData(res.data.results);
             setLoading(false);
@@ -31,7 +31,7 @@ const useData = <T>(endpoint: string) => {
 
         return () => controller.abort();
 
-    }, []);
+    }, deps ? [...deps] : []);
 
   return {data, errors, loading};
 }
