3159c42eebe24827d1920d9743b0a39a330f939f
yhKim99 <yonghoon.kim99@gmail.com>
2023-10-22 18:58:44 -0400

    filter games by genres

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -16,9 +16,12 @@ export interface Game {
     rating: number;
     background_image: string;
     parent_platforms: {platform : Platform}[];
-    metacritic: number;
+    metacritic: number; 
   }
   
-  const useGame = (selectedGenre: Genre | null) => useData<Game>("/games", {params: {genres: selectedGenre?.id}}, [selectedGenre?.id]);
+  const useGame = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>("/games", {params: {
+    genres: selectedGenre?.id, 
+    parent_platforms: selectedPlatform?.id}}, 
+    [selectedGenre?.id, selectedPlatform?.id]);
 
 export default useGame
