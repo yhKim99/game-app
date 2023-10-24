ce14f12569de5960138fb30469843f3124d696b8
yhKim99 <yonghoon.kim99@gmail.com>
2023-10-23 20:56:06 -0400

    Handle the case that if no images for games

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,15 +3,21 @@ import { Game } from "../hooks/useGame";
 import PlatformIcons from "./PlatformIcons";
 import CriticsScore from "./CriticsScore";
 import getCroppedImage from "../services/url-images";
+import noImage from "../assets/Image Placeholder/no-image-placeholder-6f3882e0.webp";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
+  const image =
+    getCroppedImage(game.background_image) === ""
+      ? noImage
+      : getCroppedImage(game.background_image);
+
   return (
     <Card>
-      <Img src={getCroppedImage(game.background_image)}></Img>
+      <Img src={image}></Img>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
