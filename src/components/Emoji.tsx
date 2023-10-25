0248ca75f08b7ac5c1750848ccd13d84c97d0543
yhKim99 <yonghoon.kim99@gmail.com>
2023-10-25 14:23:25 -0400

    Alternative way to call the emoji (comment)

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,10 +1,14 @@
-import { Image } from "@chakra-ui/react";
+import { Image, ImageProps } from "@chakra-ui/react";
 import bulles from "../assets/Emojis/bulls-eye.webp";
 import meh from "../assets/Emojis/meh.webp";
 import thumbs from "../assets/Emojis/thumbs-up.webp";
 
 interface Props {
   rating: number;
+}
+
+interface Dictonary {
+  [key: number]: ImageProps;
 }
 
 const Emoji = ({ rating }: Props) => {
@@ -18,6 +22,15 @@ const Emoji = ({ rating }: Props) => {
   } else {
     source = bulles;
   }
+
+  // Index signature
+  /* const emojiMap: Dictonary = {
+    3: {src: meh, alt: 'meh'},
+    4: {src: thumbs, alt: "thumbs"},
+    5: {src: bulles, alt: "bulles"}
+  }; */
+
+  // return <Image {...emojiMap[rating]}></Image>
 
   return <Image boxSize="30px" src={source} alt="none"></Image>;
 };
